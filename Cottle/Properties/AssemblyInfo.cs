ae59306f389514702212ce50f479d23b9de28a9d
Remi Caput <git@mirari.fr>
2019-03-12 09:33:30 +0100

    Bump version to 1.4.3.0.

--- Cottle/Properties/AssemblyInfo.cs
+++ Cottle/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.2.0")]
-[assembly: AssemblyFileVersion("1.4.2.0")]
+[assembly: AssemblyVersion("1.4.3.0")]
+[assembly: AssemblyFileVersion("1.4.3.0")]
 
 [assembly: InternalsVisibleTo("Cottle.Test")]
