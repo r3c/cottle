8936cbe43513185cb28b24d14e7f726ea12e9385
Remi Caput <git@mirari.fr>
2019-06-30 18:17:39 +0200

    Enable lazy stack frame construction for faster execution.

--- Cottle/src/Stores/SimpleStore.cs
+++ Cottle/src/Stores/SimpleStore.cs
@@ -4,55 +4,56 @@ namespace Cottle.Stores
 {
     public sealed class SimpleStore : AbstractStore
     {
-        #region Constructors
+        private int depth;
+
+        private readonly Stack<HashSet<Value>> frames;
+
+        private readonly Dictionary<Value, Stack<Value>> stacks;
 
         public SimpleStore()
         {
-            _levels = new Stack<HashSet<Value>>();
-            _stacks = new Dictionary<Value, Stack<Value>>();
+            this.depth = 0;
+            this.frames = new Stack<HashSet<Value>>();
+            this.stacks = new Dictionary<Value, Stack<Value>>();
         }
-
-        #endregion
-
-        #region Attributes
-
-        private readonly Stack<HashSet<Value>> _levels;
-
-        private readonly Dictionary<Value, Stack<Value>> _stacks;
-
-        #endregion
-
-        #region Methods
 
         public override void Enter()
         {
-            _levels.Push(new HashSet<Value>());
+            // Stack frames are lazily built (see "Set" method), we just keep trace of current stack frame depth here
+            ++this.depth;
         }
 
         public override bool Leave()
         {
-            if (_levels.Count < 1)
+            if (this.depth < 1)
                 return false;
 
-            foreach (var name in _levels.Pop())
-                if (_stacks.TryGetValue(name, out var stack))
-                {
-                    if (stack.Count < 2)
-                        _stacks.Remove(name);
-                    else
-                        stack.Pop();
-                }
+            // Early exit if no stack frames were allocated at parent depth level
+            if (--this.depth >= this.frames.Count)
+                return true;
+
+            // Delete all current stack frame symbols
+            foreach (var name in this.frames.Pop())
+            {
+                if (!this.stacks.TryGetValue(name, out var stack))
+                    continue;
+
+                if (stack.Count < 2)
+                    this.stacks.Remove(name);
+                else
+                    stack.Pop();
+            }
 
             return true;
         }
 
         public override void Set(Value symbol, Value value, StoreMode mode)
         {
-            if (!_stacks.TryGetValue(symbol, out var stack))
+            if (!this.stacks.TryGetValue(symbol, out var stack))
             {
                 stack = new Stack<Value>();
 
-                _stacks[symbol] = stack;
+                this.stacks[symbol] = stack;
             }
 
             switch (mode)
@@ -64,7 +65,12 @@ namespace Cottle.Stores
                     break;
 
                 case StoreMode.Local:
-                    if (_levels.Count > 0 && !_levels.Peek().Add(symbol))
+                    // Lazily create stack frames to current depth
+                    while (this.depth > this.frames.Count)
+                        this.frames.Push(new HashSet<Value>());
+
+                    // Erase symbol if it previously existed in current stack frame
+                    if (this.frames.Count > 0 && !this.frames.Peek().Add(symbol))
                         stack.Pop();
 
                     break;
@@ -75,7 +81,7 @@ namespace Cottle.Stores
 
         public override bool TryGet(Value symbol, out Value value)
         {
-            if (_stacks.TryGetValue(symbol, out var stack) && stack.Count > 0)
+            if (this.stacks.TryGetValue(symbol, out var stack) && stack.Count > 0)
             {
                 value = stack.Peek();
 
@@ -86,7 +92,5 @@ namespace Cottle.Stores
 
             return false;
         }
-
-        #endregion
     }
 }
