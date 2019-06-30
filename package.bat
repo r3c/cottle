a53ac9a306ffe42d1eebdd9643cc80934c0d4498
Remi Caput <git@mirari.fr>
2019-06-30 18:19:12 +0200

    Replace packaging script.

--- package.bat
+++
@@ -1,1 +0,0 @@
-dotnet pack -c Release Cottle/Cottle.csproj
---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+base="$(dirname "$0")"
+
+# Retreive latest version from current HEAD
+version="$(git --work-tree "$base" tag --points-at HEAD)"
+
+if [ -z "$version" ]; then
+	echo >&2 "error: current HEAD doesn't point to a tag"
+	exit 1
+fi
+
+dotnet pack -c Release "$base/Cottle/Cottle.csproj"
