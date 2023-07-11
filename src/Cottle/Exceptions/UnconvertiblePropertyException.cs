67c611768aba4405b76573ad6654c26c8037ba7d
Aviad Pineles <paviad2@gmail.com>
2023-07-11 09:08:59 +0300

    feat: better feedback when unable to use reflection to create context

---
+++ src/Cottle/Exceptions/UnconvertiblePropertyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cottle.Exceptions {
+    public class UnconvertiblePropertyException : Exception {
+        public UnconvertiblePropertyException(PropertyInfo propertyInfo, Exception? innerException = null)
+            : base($"Unable to create converter for property {propertyInfo}", innerException) {
+            PropertyInfo = propertyInfo;
+        }
+
+        public PropertyInfo PropertyInfo { get; set; }
+    }
+}
