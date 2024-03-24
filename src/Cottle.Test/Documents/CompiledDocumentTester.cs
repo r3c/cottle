a3ebc9eb89eec18c78abb32a01da0ce9237bf406
Remi Caput <979446+r3c@users.noreply.github.com>
2024-03-24 21:18:36 +0100

    Add new test on `wrap` command with custom function. (#223)

--- src/Cottle.Test/Documents/CompiledDocumentTester.cs
+++ src/Cottle.Test/Documents/CompiledDocumentTester.cs
@@ -20,8 +20,16 @@ namespace Cottle.Test.Documents
         }
 
         [Test]
+        [TestCase("{wrap ucase:{'a'}{unwrap:{'b'}}{'c'}}", "AbC")]
+        [TestCase("{wrap ucase:{wrap ord:{'a'}{unwrap:{'b'}}{'c'}}}", "97B99")]
+        public void Render_StatementUnwrap_CancelNearestWrap(string command, string expected)
+        {
+            AssertOutput(command, default, DocumentTester.CreateContextWithBuiltins("ord", "ucase"), expected);
+        }
+
+        [Test]
         [TestCase("{wrap ord:{wrap ucase:{'a'}{unwrap:{'b'}{unwrap:{'c'}}{'d'}}{'e'}}}", "6598c10069")]
-        public void Render_StatementUnwrap_Nested(string command, string expected)
+        public void Render_StatementUnwrap_NestedModifiers(string command, string expected)
         {
             AssertOutput(command, default, DocumentTester.CreateContextWithBuiltins("ord", "ucase"), expected);
         }
@@ -29,15 +37,31 @@ namespace Cottle.Test.Documents
         [Test]
         [TestCase("{unwrap:hello}", "hello")]
         [TestCase("{unwrap:{'hello'}}", "hello")]
-        public void Render_StatementUnwrap_Nothing(string command, string expected)
+        public void Render_StatementUnwrap_NothingToCancel(string command, string expected)
         {
             AssertOutput(command, default, Context.Empty, expected);
         }
 
         [Test]
-        [TestCase("{wrap ucase:{'a'}{unwrap:{'b'}}{'c'}}", "AbC")]
-        [TestCase("{wrap ucase:{wrap ord:{'a'}{unwrap:{'b'}}{'c'}}}", "97B99")]
-        public void Render_StatementUnwrap_Single(string command, string expected)
+        [TestCase("{wrap ucase:test}", "test")]
+        [TestCase("{wrap ucase:{'test'}}", "TEST")]
+        public void Render_StatementWrap_EchoNotLiteral(string command, string expected)
+        {
+            AssertOutput(command, default, DocumentTester.CreateContextWithBuiltins("ucase"), expected);
+        }
+
+        [Test]
+        [TestCase("{set f(x) to:{return x + 1}}{wrap f:{1}}", "2")]
+        public void Render_StatementWrap_FunctionAsModifier(string command, string expected)
+        {
+            AssertOutput(command, default, DocumentTester.CreateContextWithBuiltins(), expected);
+        }
+
+        [Test]
+        [TestCase("{wrap ord:{wrap ucase:{'a'}}}", "65")]
+        [TestCase("{wrap ucase:{wrap ord:{'a'}}}", "97")]
+        [TestCase("{wrap ucase:{'a'}{wrap ord:{'b'}}{'c'}}", "A98C")]
+        public void Render_StatementWrap_NestedModifiers(string command, string expected)
         {
             AssertOutput(command, default, DocumentTester.CreateContextWithBuiltins("ord", "ucase"), expected);
         }
@@ -45,24 +69,7 @@ namespace Cottle.Test.Documents
         [Test]
         [TestCase("{set f(x) to:{x}}{wrap ucase:{f('a')}}", "A")]
         [TestCase("{wrap ucase:{set f(x) to:{x}}}{f('a')}", "a")]
-        public void Render_StatementWrap_Function(string command, string expected)
-        {
-            AssertOutput(command, default, DocumentTester.CreateContextWithBuiltins("ucase"), expected);
-        }
-
-        [Test]
-        [TestCase("{wrap ord:{wrap ucase:{'a'}}}", "65")]
-        [TestCase("{wrap ucase:{wrap ord:{'a'}}}", "97")]
-        [TestCase("{wrap ucase:{'a'}{wrap ord:{'b'}}{'c'}}", "A98C")]
-        public void Render_StatementWrap_Nested(string command, string expected)
-        {
-            AssertOutput(command, default, DocumentTester.CreateContextWithBuiltins("ord", "ucase"), expected);
-        }
-
-        [Test]
-        [TestCase("{wrap ucase:test}", "test")]
-        [TestCase("{wrap ucase:{'test'}}", "TEST")]
-        public void Render_StatementWrap_Single(string command, string expected)
+        public void Render_StatementWrap_ThroughInvokeNotDeclare(string command, string expected)
         {
             AssertOutput(command, default, DocumentTester.CreateContextWithBuiltins("ucase"), expected);
         }
